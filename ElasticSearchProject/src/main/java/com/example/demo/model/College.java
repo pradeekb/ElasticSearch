9fa9d7b9816591c1c1512722ccef7267a3b59cd9
pradeekb <kbpradeepece@gmail.com>
2018-09-20 14:59:46 +0530

    removed unwanted comments in pojo classes

--- ElasticSearchProject/src/main/java/com/example/demo/model/College.java
+++ ElasticSearchProject/src/main/java/com/example/demo/model/College.java
@@ -2,14 +2,12 @@ package com.example.demo.model;
 
 public class College {
 	
-	//student object
 	private StudentDetail department;
 
 	public StudentDetail getDepartment() {
 		return department;
 	}
 
-	//student detail new comment
 	public void setDepartment(StudentDetail department) {
 		this.department = department;
 	}
